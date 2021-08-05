2e4516deedb146da397890830eca96466cfbff2d
mashuaim16 <49493623+mashuaim16@users.noreply.github.com>
2021-08-05 16:19:52 +0800

    Add files via upload

---
+++ always_OL.py
@@ -0,0 +1,26 @@
+import pyautogui
+# import os
+# os.system('pip install pyautogui')    # auto pip install this package
+# pyautogui.dragTo(100, 150)
+# pyautogui.dragRel(0, 10)              # drag mouse 10 pixels down
+# pyautogui.moveRel(0, 50)              # move mouse 10 pixels down
+def move_mouse():
+    pyautogui.moveTo(1600, 150)
+    position_v = 150
+    position_h = 1600
+    for i in range(8):     # move mouse down
+        position_v =+ 100
+        pyautogui.moveRel(0, position_v, duration=0.5)
+    for i in range(10):    # move mouse left
+        position_h =- 120
+        pyautogui.moveRel(position_h, 0, duration=0.5)
+    for i in range(8):    # move mouse up
+        position_v =- 100
+        pyautogui.moveRel(0, position_v,duration=0.5)
+    for i in range(10):    # move mouse right
+        position_h =+ 120
+        pyautogui.moveRel(position_h, 0,duration=0.5)
+    
+
+if __name__ == '__main__':
+    move_mouse()
